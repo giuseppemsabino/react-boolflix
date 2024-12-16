60684378cef65c9d1731dcde3f785311768df529
giuseppemsabino <giuseppemendoza004@gmail.com>
2024-12-16 16:27:25 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+
+//layout
+
+//contexts
+
+//pages
+
+
+function App() {
+  
+
+  return (
+   <BrowserRouter>
+   <Routes>
+    
+   </Routes>
+   </BrowserRouter>
+  )
+}
+
+export default App
