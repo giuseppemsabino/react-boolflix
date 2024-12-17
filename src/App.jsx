258955ebc82247b746b23ae95a94303f620d15de
giuseppemsabino <giuseppemendoza004@gmail.com>
2024-12-17 19:06:38 +0100

    now u can see the movies

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,44 @@
 import { useState } from "react";
 import { useMovieContext } from "./context/MovieContext";
 
-
-
-function App(){
-  const {searchMovies}= useMovieContext();
-  const [userTerm, setUserTerm] = useState('');
+function App() {
+  const { movies, searchMovies } = useMovieContext();
+  const [userTerm, setUserTerm] = useState("");
 
   const handleUserTermChange = (e) => {
-    setUserTerm(e.target.value)
-  }
+    setUserTerm(e.target.value);
+  };
 
   const handleSearchOnSubmit = (e) => {
     e.preventDefault();
-
     searchMovies(userTerm);
-  }
+  };
 
-
-  return(
+  return (
     <>
-    <header>
-      <form onSubmit={handleSearchOnSubmit}>
-        <input value={userTerm} onChange={handleUserTermChange} type="text" />
-        <button>Cerca</button>
-      </form>
-    </header>
-    <main>
-    <h1>hello</h1>
-    </main>
+      <header>
+        <form onSubmit={handleSearchOnSubmit}>
+          <input value={userTerm} onChange={handleUserTermChange} type="text" />
+          <button>Cerca</button>
+        </form>
+      </header>
+      <main>
+        <h1>Movies List</h1>
+        {movies.length > 0 ? (
+          movies.map((movie) => (
+            <ul key={movie.id}>
+              <li>titolo: {movie.title}</li>
+              <li>Titolo originale: {movie.original_title}</li>
+              <li>lingua: {movie.original_language}</li>
+              <li>Voto: {movie.vote_average}</li>
+            </ul>
+          ))
+        ) : (
+          <p>Non ci sono risultati</p>
+        )}
+      </main>
     </>
-  )
+  );
 }
-export default App;
 
+export default App;
--- src/context/MovieContext.jsx
+++ src/context/MovieContext.jsx
@@ -1,35 +1,41 @@
-import { createContext, useContext} from "react";
+import { createContext, useContext, useState } from "react";
 
 const MovieContext = createContext();
 
-export const MovieContextProvider = ({children}) => {
+export const MovieContextProvider = ({ children }) => {
   const url = import.meta.env.VITE_BASE_URL;
-  const apiKey= import.meta.env.VITE_API_KEY
+  const apiKey = import.meta.env.VITE_API_KEY;
 
-  const fetchMovies= (term) => {
+  const fetchMovies = (term) => {
     const fetchConfig = {
-      method: 'GET',
+      method: "GET",
       headers: {
-        accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNjMyODI0NjBkZTE5Y2ZmYTc1ZWZiZDk2MzEyN2Q5OCIsIm5iZiI6MTczNDM2MzczNy42NzkwMDAxLCJzdWIiOiI2NzYwNGE1OWE4ZDBlYjU2MGZhYTgxZjkiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.Jql3mTZw7PDMFIy-3uP7w-t1kGp9r7AgCgS1ZChAAzo'
-      }
+        accept: "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNjMyODI0NjBkZTE5Y2ZmYTc1ZWZiZDk2MzEyN2Q5OCIsIm5iZiI6MTczNDM2MzczNy42NzkwMDAxLCJzdWIiOiI2NzYwNGE1OWE4ZDBlYjU2MGZhYTgxZjkiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.Jql3mTZw7PDMFIy-3uP7w-t1kGp9r7AgCgS1ZChAAzo",
+      },
     };
     fetch(`${url}/search/movie?query=${term}`, fetchConfig)
-    .then(res => res.json())
-    .then(data => {
-      console.log(data);
-      
-    })
-    alert('hai ceracto: ' + term);
+      .then((res) => res.json())
+      .then((data) => {
+        setGlobalData((prevData) => ({
+          ...prevData, 
+          movies: data.results, 
+        }));
+      });
+    alert("Hai cercato: " + term);
   };
 
-  const globalData ={
-    apiUrl : import.meta.env.VITE_BASE_URL,
+  const [globalData, setGlobalData] = useState({
+    apiUrl: import.meta.env.VITE_BASE_URL,
     movies: [],
     searchMovies: fetchMovies,
     series: [],
-  }
-  return <MovieContext.Provider value={globalData}>{children}</MovieContext.Provider>
-}
+  });
 
-export const useMovieContext = () => useContext(MovieContext); 
+  return (
+    <MovieContext.Provider value={globalData}>{children}</MovieContext.Provider>
+  );
+};
+
+export const useMovieContext = () => useContext(MovieContext);
