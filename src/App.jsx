95474196f2df13ba0066a645b1e49f4aed51a996
giuseppemsabino <giuseppemendoza004@gmail.com>
2024-12-17 18:38:47 +0100

    api conected succesfully and better handle

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,31 @@ import { useMovieContext } from "./context/MovieContext";
 
 
 function App(){
-  const {apiUrl} = useMovieContext();
-  const {useTerm, setUseTerm} = useState('');
+  const {searchMovies}= useMovieContext();
+  const [userTerm, setUserTerm] = useState('');
 
   const handleUserTermChange = (e) => {
-    setUseTerm(e.target.value)
+    setUserTerm(e.target.value)
   }
 
   const handleSearchOnSubmit = (e) => {
     e.preventDefault();
+
+    searchMovies(userTerm);
   }
 
 
   return(
     <>
     <header>
-      <form >
-        <input value={useTerm} onChange={handleUserTermChange} type="text" />
+      <form onSubmit={handleSearchOnSubmit}>
+        <input value={userTerm} onChange={handleUserTermChange} type="text" />
         <button>Cerca</button>
       </form>
     </header>
+    <main>
     <h1>hello</h1>
-    <p>{apiUrl}</p>
+    </main>
     </>
   )
 }
--- src/context/MovieContext.jsx
+++ src/context/MovieContext.jsx
@@ -3,12 +3,33 @@ import { createContext, useContext} from "react";
 const MovieContext = createContext();
 
 export const MovieContextProvider = ({children}) => {
+  const url = import.meta.env.VITE_BASE_URL;
+  const apiKey= import.meta.env.VITE_API_KEY
+
+  const fetchMovies= (term) => {
+    const fetchConfig = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNjMyODI0NjBkZTE5Y2ZmYTc1ZWZiZDk2MzEyN2Q5OCIsIm5iZiI6MTczNDM2MzczNy42NzkwMDAxLCJzdWIiOiI2NzYwNGE1OWE4ZDBlYjU2MGZhYTgxZjkiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.Jql3mTZw7PDMFIy-3uP7w-t1kGp9r7AgCgS1ZChAAzo'
+      }
+    };
+    fetch(`${url}/search/movie?query=${term}`, fetchConfig)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      
+    })
+    alert('hai ceracto: ' + term);
+  };
+
   const globalData ={
     apiUrl : import.meta.env.VITE_BASE_URL,
     movies: [],
-    series: []
+    searchMovies: fetchMovies,
+    series: [],
   }
   return <MovieContext.Provider value={globalData}>{children}</MovieContext.Provider>
 }
 
-export const useMovieContext = ()=> useContext(MovieContext)
+export const useMovieContext = () => useContext(MovieContext); 
